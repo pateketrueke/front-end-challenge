2a805ce2c4a1f285635a135cbe86182af2e25e0c
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2018-05-24 01:44:09 -0500

    Working component for Trades; reuse it later

--- src/app/assets/components/trades-widget/_/index.jsx
+++ src/app/assets/components/trades-widget/_/index.jsx
@@ -1,8 +1,11 @@
 import Util from '../../_/Util';
 
+// FIXME: abstract as much as possible and reuse!
+
 function parseData(payload) {
   return payload.map(item => ({
-    className: item.marker_side ? 'up' : 'down',
+    is: item.maker_side === 'sell' ? 'up' : 'down',
+    key: item.tid,
     time: moment(item.created_at).format('H:mm:ss'),
     price: Util.formatNumber(item.book.split('_'), item.price),
     amount: Util.priceFormat(item.amount),
@@ -15,33 +18,160 @@ function fetchData(book) {
     .then(data => parseData(data.payload));
 }
 
+function save(target) {
+  return ref => {
+    target.$el = ref;
+  };
+}
+
+function add(target, className) {
+  return () => {
+    target.$el.classList.add(className);
+  };
+}
+
+function rm(target, className) {
+  return () => {
+    target.$el.classList.remove(className);
+  };
+}
+
+function toggle(props, target, className) {
+  return e => {
+    e.preventDefault();
+
+    const offset = props.values.indexOf(target);
+
+    if (offset !== -1) {
+      props.unset(offset).$el.classList.remove(className);
+      return;
+    }
+
+    props.set(target).$el.classList.add(className);
+  };
+}
+
+function Highlight(props) {
+  const suffix = Array.from({
+    length: 11 - String(props.value).length
+  }).join('0');
+
+  const matches = String(props.value).match(/^(.+?)(0+)$/);
+
+  if (!matches) {
+    return <span><span className='hi'>{props.value}</span>{suffix}</span>;
+  }
+
+  return (
+    <span><span className='hi'>{matches[1]}</span>{matches[2]}{suffix}</span>
+  );
+}
+
+function Wrapper(props) {
+  return (
+    <div className='md-push v-scroll'>{props.children}</div>
+  );
+}
+
+function Table(props) {
+  return (
+    <table className='full-width' cellPadding={0} cellSpacing={0}>{props.children}</table>
+  );
+}
+
+function Caption({ isLoading }) {
+  return (
+    <caption className='pad caps align-left'>
+      Últimos trades{isLoading ? '...' : ''}
+    </caption>
+  );
+}
+
+function Header(props) {
+  return (
+    <thead>
+      <tr className='caps'>
+        <th className='pad align-left'>Hora</th>
+        <th className='pad align-right coin mxn'>Precio</th>
+        <th className='pad align-right coin btc'>Monto</th>
+      </tr>
+    </thead>
+  );
+}
+
+function Body(props) {
+  return (
+    <tbody>{props.items.map(item => (
+      <tr key={item.key} ref={save(item)} onClick={toggle(props, item, 'sel')}>
+        <td className='pad align-left'><span className='hi'>{item.time}</span></td>
+        <td className='pad align-right'><span className={item.is}>{item.price}</span></td>
+        <td className='pad align-right'><Highlight value={item.amount} /></td>
+      </tr>
+    ))}</tbody>
+  );
+}
+
 export default class extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       loading: true,
+      selected: [],
     };
   }
 
   componentDidMount() {
     fetchData('btc_mxn')
       .then(result => {
-        console.log('>>>', result);
-
         this.setState({
-          loading: false,
           trades: result,
+          loading: false,
+          selected: [],
         });
       });
   }
 
+  unselectItem(offset) {
+    const node = this.state.selected[offset];
+    const copy = this.state.selected;
+
+    copy.splice(offset, 1);
+
+    this.setState({
+      selected: copy,
+    });
+
+    return node;
+  }
+
+  selectItem(node) {
+    const copy = this.state.selected;
+
+    this.setState({
+      selected: copy.concat(copy.indexOf(node) === -1 ? node : []),
+    });
+
+    return node;
+  }
+
   render() {
-    const { loading } = this.state;
+    const { selected, loading, trades } = this.state;
 
-    if (loading) {
-      return <div>Cargando últimos trades...</div>;
-    }
-
-    return <div>WIP</div>;
+    return (
+      <Wrapper>
+        <Table>
+          <Caption isLoading={loading} />
+          <Header />
+          {!loading ? (
+            <Body
+              set={node => this.selectItem(node)}
+              unset={offset => this.unselectItem(offset)}
+              items={trades}
+              values={selected}
+            />
+          ) : null}
+        </Table>
+      </Wrapper>
+    );
   }
 }
