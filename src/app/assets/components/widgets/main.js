98487147d07dff929d6e7c58111be9be7a0267b7
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2018-06-01 02:38:18 -0500

    Load DeepChart; WIP

--- src/app/assets/components/widgets/main.js
+++ src/app/assets/components/widgets/main.js
@@ -1,6 +1,7 @@
 import CustomTable from './_/CustomTable';
 import CustomList from './_/CustomList';
 import LineChart from './_/LineChart';
+import DeepChart from './_/DeepChart';
 import CandleChart from './_/CandleChart';
 
 import './_/scripts';
@@ -9,5 +10,6 @@ document.currentScript.exports = {
   CustomTable,
   CustomList,
   LineChart,
+  DeepChart,
   CandleChart,
 };
